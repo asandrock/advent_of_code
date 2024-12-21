a08a0b7dfd59f58d5f6883dcd8c6727c87b3a050
Alexander Sandrock <asandrock@uni-wuppertal.de>
2024-12-21 21:23:07 +0100

    New implementation for Dec 09, but still not correct solution

--- 2024/09/main1.f90
+++ 2024/09/main1.f90
@@ -2,55 +2,76 @@ program main1
   use stringifor
   implicit none
   character(len=*), parameter :: filename = 'input'
-  type(string) :: input, system, n_block, n_free
+  type(string) :: input, system, n_block
   character(len=80) :: id
-  type(string), allocatable :: file_blocks(:)
-  integer :: j, k, checksum
+  integer(kind=i4p) :: j, k, l, length
+  integer(kind=i4p) :: checksum
+  character(len=1), dimension(:), allocatable :: aux
+
+  integer, allocatable, dimension(:) :: file_system
 
   call input%read_file(file=filename)
+  !input = '12345'
+  !input = '2333133121414131402'
 
   ! decode input
   system = ''
-  do j = 0, input%len()/2 - 1
-    write (unit=id, fmt=*) j
-    n_block = input%slice(2*j + 1, 2*j + 1)
-    n_free = input%slice(2*j + 2, 2*j + 2)
-    system = system // repeat(trim(adjustl(id)) // ',' , n_block%to_number(kind=1_i1p)) &
-    & // repeat('.,', n_free%to_number(kind=1_I1P))
+  do j = 1, input%len()
+    write (unit=id, fmt=*) (j - 1)/2
+    n_block = input%slice(j, j)
+    if (mod(j, 2) == 1) then
+      system = system // repeat(trim(adjustl(id)) // ',', n_block%to_number(kind=1_i4p))
+    else
+      system = system // repeat('.,', n_block%to_number(kind=1_i4p))
+    end if
   end do
-  call system%split(tokens=file_blocks, sep=',')
-  !print *, file_blocks
+
+  !print *, system
+  length = system%len()
+  write (*,*) length
+  allocate(aux(length))
+  do j = 1, length
+    aux(j) = system%slice(j, j)
+  end do
+  write (*,*) count(aux == ',')
+
+  allocate(file_system(count(aux == ',')))
+  l = 1
+  do j = 1, input%len()
+    n_block = input%slice(j, j)
+    if (mod(j, 2) == 1) then
+      do k = 1, n_block%to_number(kind=1_i4p)
+        file_system(l) = (j - 1)/2
+        l = l + 1
+      end do
+    else
+      do k = 1, n_block%to_number(kind=1_i4p)
+        file_system(l) = -1
+        l = l + 1
+      end do
+    end if
+  end do
 
   ! defragmentation
   do
-    do j = 1, size(file_blocks) ! left-most empty space
-      if (file_blocks(j) == '.') then
-        exit
-      end if
+    do j = 1, size(file_system)
+      if (file_system(j) == -1) exit
     end do
-    do k = size(file_blocks), 1, -1 ! right-most file block
-      if (file_blocks(k) /= '.') then
-        exit
-      end if
+    do k = size(file_system), 1, -1
+      if (file_system(k) /= -1) exit
     end do
-    !print *, j, k
-    if (j > k) then
-      exit
-    else
-      file_blocks(j) = file_blocks(k)
-      file_blocks(k) = '.'
-    end if
-    !print *, file_blocks
+    if (j > k) exit
+
+    file_system(j) = file_system(k)
+    file_system(k) = -1
+    !print *, file_system
   end do
 
   ! calculate checksum
   checksum = 0
-  do j = 0, size(file_blocks)
-    if (file_blocks(j) == '.') then
-      exit
-    else
-      checksum = checksum + (j - 1)*file_blocks(j)%to_number(kind=1_I1P)
-    end if
+  do j = 1, size(file_system) - count(file_system == -1)
+    !if (file_system(j) == -1) exit
+    checksum = checksum + (j - 1)*file_system(j)
   end do
   print *, checksum
 end program main1
